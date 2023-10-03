4c47720ca184d83098d23fa6ec1a1f4f79b8c496
Rodrimansidub14 <man22611@uvg.edu.gt>
2023-10-03 15:01:55 -0600

    .

--- Lab5-03102023/app/build.gradle.kts
+++ Lab5-03102023/app/build.gradle.kts
@@ -1,27 +1,26 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+
 }
 
 android {
-    namespace = "com.example.lab5_03102023"
-    compileSdk = 33
+    namespace = "com.example.loginapplab4"
+
+    compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.lab5_03102023"
         minSdk = 29
-        targetSdk = 33
+        targetSdk = 31
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
+        vectorDrawables.useSupportLibrary = true
     }
 
     buildTypes {
-        release {
+        getByName("release") {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
@@ -36,33 +35,31 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    buildFeatures {
-        compose = true
-    }
+    buildFeatures.compose = true
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
+        kotlinCompilerExtensionVersion = "1.4.2" // Update this to match your Compose version
     }
 }
 
 dependencies {
+    implementation("androidx.compose.runtime:runtime-rxjava3:1.5.2")
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("io.coil-kt:coil-compose:1.4.0")
+    implementation("androidx.navigation:navigation-compose:2.7.3")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.2")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation(platform("androidx.compose:compose-bom:2023.1.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material3:material3:1.1.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
--- Lab5-03102023/app/src/main/java/com/example/lab5_03102023/MainActivity.kt
+++ Lab5-03102023/app/src/main/java/com/example/lab5_03102023/MainActivity.kt
@@ -1,46 +1,103 @@
 package com.example.lab5_03102023
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.lab5_03102023.ui.theme.Lab503102023Theme
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+interface ShibeApi {
+    @GET("shibes?count=1&urls=true&httpsUrls=true")
+    suspend fun getRandomShibe(): List<String>
+}
+
+private const val BASE_URL = "http://shibe.online/api/"
+
+val shibeApi: ShibeApi = Retrofit.Builder()
+    .baseUrl(BASE_URL)
+    .addConverterFactory(GsonConverterFactory.create())
+    .build()
+    .create(ShibeApi::class.java)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Lab503102023Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            ShibeScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ShibeScreen() {
+    var imageUrl by remember { mutableStateOf<String?>(null) }
+    var errorMessage by remember { mutableStateOf<String?>(null) }
+    val coroutineScope = rememberCoroutineScope()
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = {
+            coroutineScope.launch {
+                try {
+                    val response = shibeApi.getRandomShibe()
+                    imageUrl = response.firstOrNull()
+                    errorMessage = null  // reset the error message
+                } catch (e: Exception) {
+                    imageUrl = null  // reset the image
+                    errorMessage = "Error fetching image. Please try again."
+                }
+            }
+        }) {
+            Text(text = "Mostrar Shibe")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        errorMessage?.let {
+            Text(text = it, color = Color.Red)
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+
+        imageUrl?.let {
+            Image(
+                painter = rememberImagePainter(data = it),
+                contentDescription = null,
+                modifier = Modifier.size(200.dp)
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true)
+
+@Preview
 @Composable
-fun GreetingPreview() {
-    Lab503102023Theme {
-        Greeting("Android")
-    }
+fun PreviewShibeScreen() {
+    ShibeScreen()
 }
