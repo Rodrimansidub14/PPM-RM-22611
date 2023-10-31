ba0b4f99e1bf114684a6ed54c4cf98cbfee4f352
Rodrimansidub14 <man22611@uvg.edu.gt>
2023-10-31 09:35:48 -0600

    lab11

---
+++ Lab10-24102023/app/src/main/java/com/example/lab10_24102023/ui/theme/SuccesfulLoginScrn.kt
@@ -0,0 +1,49 @@
+package com.example.lab10_24102023.ui.theme
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.google.firebase.auth.FirebaseUser
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.material3.Text
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.ui.Alignment
+import com.google.firebase.auth.FirebaseAuth
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@ExperimentalMaterial3Api
+@Composable
+fun HomeScreen(user: FirebaseUser?) {
+    MaterialTheme {
+        Scaffold(
+            content = {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "Hello, ${user?.email}")
+                }
+            }
+        )
+    }
+}
+
+class HomeActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HomeScreen(FirebaseAuth.getInstance().currentUser)
+        }
+    }
+}
